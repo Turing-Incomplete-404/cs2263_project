63bddf0bf734292cdce28f215648099f8ee63750
Eric Hill <hilleri2@isu.edu>
2022-04-09 17:19:21 -0600

    remove tile from hand test

--- app/src/test/PlayerTest.groovy
+++ app/src/test/PlayerTest.groovy
@@ -1,5 +1,6 @@
 import cs2263_project.GameInfo
 import cs2263_project.Player
+import cs2263_project.Tile
 import jdk.jfr.Category
 import spock.lang.Specification
 
@@ -39,5 +40,24 @@ class PlayerTest extends Specification {
 
         then:
         player.stockAmount(corporation) != -1
+    }
+
+    def "check removing tile from hand"(){
+        given:
+        Tile tile1 = new Tile(1,1)
+        Tile tile2 = new Tile(2,1)
+        Tile tile3 = new Tile(1,2)
+        Tile tile4 = new Tile(2,2)
+        player.addtile(tile1)
+        player.addtile(tile2)
+        player.addtile(tile3)
+        player.addtile(tile4)
+
+        when:
+        player.removeTile(tile2)
+
+        then:
+        int i = 0;
+        if (player.hand[0].equals(tile1) && player.hand[1].equals(tile3) && player.hand[2].equals(tile4)){return true;}
     }
 }
