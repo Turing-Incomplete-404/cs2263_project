77527be67f9cf6cf0d595937c3f588313975d91a
Eric Hill <hilleri2@isu.edu>
2022-04-10 11:42:23 -0600

    creating StartUI tests

---
+++ app/src/test/groovy/cs2263_project/StartUITest.groovy
@@ -0,0 +1,43 @@
+package cs2263_project
+
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+import org.testfx.framework.spock.ApplicationSpec;
+
+class StartUITest extends ApplicationSpec {
+    /*
+    @Override
+    void start(Stage stage) throws Exception {
+
+    }
+
+     */
+
+    @Override
+    void start(Stage stage) {
+        Button button = new Button('click me!')
+        button.setOnAction { button.setText('clicked!') }
+        stage.setScene(new Scene(new StackPane(button), 100, 100))
+        stage.show()
+    }
+
+    @Override
+    void stop() throws Exception {
+        FxToolkit.hideStage()
+    }
+
+    def "should contain button"() {
+        expect:
+        verifyThat('.button', hasText('click me!'))
+    }
+
+    def "should click on button"() {
+        when:
+        clickOn(".button")
+
+        then:
+        verifyThat('.button', hasText('clicked!'))
+    }
+}
