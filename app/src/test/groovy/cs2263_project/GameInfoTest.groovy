9d2b5c8d4c1e049911b65a6259523814182cba30
Eric Hill <hilleri2@isu.edu>
2022-04-09 21:03:07 -0600

    gameinfo tests

--- app/src/test/groovy/cs2263_project/GameInfoTest.groovy
+++ app/src/test/groovy/cs2263_project/GameInfoTest.groovy
@@ -3,5 +3,43 @@ package cs2263_project
 import spock.lang.Specification
 
 class GameInfoTest extends Specification {
+    def "check primary bonus for corps"(){
+        given:
+        GameInfo gameInfo = new GameInfo()
 
+        expect:
+        gameInfo.getPrimaryBonus(GameInfo.Corporations[0],3) == 3000
+        gameInfo.getPrimaryBonus(GameInfo.Corporations[2],5) == 6000
+        gameInfo.getPrimaryBonus(GameInfo.Corporations[5],20) == 9000
+    }
+
+    def "check costs for corps"(){
+        given:
+        GameInfo gameInfo = new GameInfo()
+
+        expect:
+        gameInfo.getCost(GameInfo.Corporations[0],80) == 1000
+        gameInfo.getCost(GameInfo.Corporations[2],80) == 1100
+        gameInfo.getCost(GameInfo.Corporations[5],80) == 1200
+    }
+
+    def "check secondary bonus for corps"(){
+        given:
+        GameInfo gameInfo = new GameInfo()
+
+        expect:
+        gameInfo.getSecondaryBonus(GameInfo.Corporations[0],3) == 1500
+        gameInfo.getSecondaryBonus(GameInfo.Corporations[2],5) == 3000
+        gameInfo.getSecondaryBonus(GameInfo.Corporations[5],20) == 4500
+    }
+
+    def "check minimum boundaries"(){
+        given:
+        GameInfo gameInfo = new GameInfo()
+
+        expect:
+        gameInfo.getCost(GameInfo.Corporations[0],2) == 200
+        gameInfo.getCost(GameInfo.Corporations[2],2) == 300
+        gameInfo.getCost(GameInfo.Corporations[5],2) == 400
+    }
 }
